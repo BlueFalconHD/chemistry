ecf35ab09627c8251cb79f5723813157bb616cda
bluefalconhd <hjdombro@icloud.com>
2025-01-10 14:09:22 -0600

    Initial Commit

---
+++ chemistry/chemistryApp.swift
@@ -0,0 +1,17 @@
+//
+//  chemistryApp.swift
+//  chemistry
+//
+//  Created by Hayes Dombroski on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct chemistryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
